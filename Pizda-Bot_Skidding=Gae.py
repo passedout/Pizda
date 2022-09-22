774076497f4ddf1d2ee3ed7b17409f112abe2d4f
beam (aka. Karma) <94296856+RemoteTool@users.noreply.github.com>
2022-09-23 00:45:15 +0200

    Update Pizda-Bot_Skidding=Gae.py

--- Pizda-Bot_Skidding=Gae.py
+++ Pizda-Bot_Skidding=Gae.py
@@ -313,7 +313,7 @@ async def help(ctx):
 
 
 
-#####ALL Rights Reserved To Karma <3 | Github = @RemoteTool <3
+#####ALL Rights Reserved To beam <3 | Github = @RemoteTool <3
 #Current Dc server: discord.gg/feared & discord.gg/termsofservice <~~ maybe taken by some nft faggot :|
 #Telegram $~~> print("https://t.me/boobjob + t.me/tosviolators")
 
